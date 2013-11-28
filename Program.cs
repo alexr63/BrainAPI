fc897e316167300748740b72799786f805b9c5c1
alexr63 <alex@resnik.co.ua>
2013-11-28 17:49:05 +0200

    read a from clipboard and write b

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrainAPI
+{
+    class Program
+    {
+        [STAThread()]
+        static void Main(string[] args)
+        {
+            if (Clipboard.GetText() == "a")
+            {
+                Clipboard.SetText("b");
+            }
+        }
+    }
+}
